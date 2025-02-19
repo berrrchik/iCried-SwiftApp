7b23850c818c00bc366c296937ce751baa517f3d
berrrchik <berrrchik@mail.ru>
2025-02-19 17:53:52 +0300

    Initial Commit

---
+++ iCriedTodayDesign/iCriedTodayDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCriedTodayDesignApp.swift
+//  iCriedTodayDesign
+//
+//  Created by Анастасия Берчик on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct iCriedTodayDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
